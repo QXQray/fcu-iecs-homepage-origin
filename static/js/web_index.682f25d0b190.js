31fa6ecf51f6d09263ddaffd944e94c4adfca71d
Ray <D1358010@o365.fcu.edu.tw>
2026-03-31 21:43:44 +0800

    Add new images and enhance styles and scripts for improved accessibility and functionality
    - Added multiple new banner images for index and news sections.
    - Updated CSS to improve color contrast for accessibility and added styles for buttons and event dates.
    - Refactored JavaScript for Swiper initialization to ensure it only runs if the element exists, and improved modal video handling.

--- static/js/web_index.682f25d0b190.js
+++ static/js/web_index.682f25d0b190.js
@@ -1,35 +1,61 @@
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
-    slidesPerView: 'auto',
-    loop: true,
-    loopAdditionalSlides: 1,
-    watchSlidesProgress: true,
-    autoplay: {
-        delay: 2000,
-        pauseOnMouseEnter: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+var swiperElement = document.querySelector('.mySwiper');
+var swiper = null;
+
+if (swiperElement) {
+    swiper = new Swiper(".mySwiper", {
+        spaceBetween: 10,
+        slidesPerView: 'auto',
+        loop: true,
+        loopAdditionalSlides: 1,
+        watchSlidesProgress: true,
+        autoplay: {
+            delay: 2000,
+            pauseOnMouseEnter: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+}
 
 document.querySelectorAll('.index-video').forEach(element => {
     element.addEventListener('click', () => {
-        swiper.autoplay.stop();
+        if (swiper && swiper.autoplay) {
+            swiper.autoplay.stop();
+        }
         let url = element.dataset.url;
         url = url.split('v=')[1];
-        document.querySelector('#modalForVideo .modal-content').innerHTML = `<iframe src="https://www.youtube.com/embed/${url}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+        var modalContent = document.querySelector('#modalForVideo .modal-content');
+
+        if (modalContent) {
+            modalContent.innerHTML = `<iframe src="https://www.youtube.com/embed/${url}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+        }
     });
 });
 
-document.querySelector('#modalForVideo').addEventListener('hide.bs.modal', () => {
-    document.querySelector('#modalForVideo .modal-content').innerHTML = '';
-    swiper.autoplay.start();
-});
+var modalForVideo = document.querySelector('#modalForVideo');
+
+if (modalForVideo) {
+    modalForVideo.addEventListener('hide.bs.modal', () => {
+        var modalContent = document.querySelector('#modalForVideo .modal-content');
+
+        if (modalContent) {
+            modalContent.innerHTML = '';
+        }
+
+        if (swiper && swiper.autoplay) {
+            swiper.autoplay.start();
+        }
+    });
+}
 
 window.addEventListener('load', () => {
     let index_about = document.querySelector('.index-about-background');
+    if (!index_about || !index_about.dataset || !index_about.dataset.bgimg) {
+        return;
+    }
+
     let bgimg = index_about.dataset.bgimg;
     document.getElementsByClassName('index-about-background')[0].style.backgroundImage = `url(${location.origin + bgimg})`;
 });
